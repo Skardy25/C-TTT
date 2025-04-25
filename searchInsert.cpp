0560a1bc37ef5bf2575cd3504f68245d7102dc61
Skardy25 <franquitotlvbb@gmail.com>
2025-04-25 01:34:46 -0500

    Update searchInsert.cpp

--- searchInsert.cpp
+++ searchInsert.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     const int searchInsert(vector<int>& nums, const int &target) {
         int rigth = nums.size()-1;
-        int left = 0;
+        int left=0;
         while(left <= rigth){
-            int mid =left + (rigth - left)/2;
+            int mid =left + ((rigth - left) >> 1);
             if(nums[mid] > target){
                 rigth--;
             }else if(nums[mid] < target){
